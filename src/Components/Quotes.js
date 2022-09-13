bdb120b057f2bfd61e367ff8d99b7b7f35f4d560
JakeSnellin <jake_snellin@hotmail.co.uk>
2022-09-13 18:37:50 +0100

    type mobile display error fixed

--- src/Components/Quotes.js
+++ src/Components/Quotes.js
@@ -12,7 +12,7 @@ export function Quotes (props){
     const quoteStyle ={
         width: width >= desktopBreakpoint ? '625px' : width >= tabletBreakpoint ? '555px' : '100%',
         fontSize: width >= desktopBreakpoint ? '64px' : width >= tabletBreakpoint ? '48px' : '32px',
-        fontFamily: 'Roc Grotesk',
+        fontFamily: 'roc-grotesk',
         fontStyle: 'normal',
         fontWeight: '750', 
         marginBottom: '28px'
@@ -21,7 +21,7 @@ export function Quotes (props){
     const sourceStyle = {
         width: width >= desktopBreakpoint ? '625px' : width >= tabletBreakpoint ? '555px' : '100%',
         fontSize: width >= desktopBreakpoint ? '34px' : width >= tabletBreakpoint ? '24px' : '20px',
-        fontFamily: 'Roc Grotesk',
+        fontFamily: 'roc-grotesk',
         fontStyle: 'normal',
         fontWeight: '400',
         marginBottom: '81px'
@@ -34,7 +34,6 @@ export function Quotes (props){
     return(
 
         <div style={quoteContainer}>
-            {console.log(props.quotes)}
             {props.quotes.map((text, index)=>{
             return index % 2 === 0 ? <p key={index} style={quoteStyle}>{text}</p> : <p key={index} style={sourceStyle}>{text}</p>
         })}
